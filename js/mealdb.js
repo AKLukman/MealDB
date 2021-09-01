97f016e97f568c74ee456d43be0a3e0643d4f9bc
AKLukman <abulkashemlukman@gmail.com>
2021-09-01 14:10:47 +0600

    first commit

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,20 +1,20 @@
 // Display food
 const displayFood = (searchText) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => dispalyFishMeals(data.meals))
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => dispalyFishMeals(data.meals))
 }
 
 displayFood('fish');
 
 const dispalyFishMeals = meals => {
-    // console.log(meals);
-    const fishInfo = document.getElementById('fish-info');
-    meals.forEach(meal => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+  // console.log(meals);
+  const fishInfo = document.getElementById('fish-info');
+  meals.forEach(meal => {
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
         <div class="card">
         <img width="200px" src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -23,8 +23,8 @@ const dispalyFishMeals = meals => {
         </div>
       </div>
         `
-        fishInfo.appendChild(div);
-    })
+    fishInfo.appendChild(div);
+  })
 
 
 }
@@ -32,32 +32,43 @@ const dispalyFishMeals = meals => {
 // Search Food
 
 const searchFood = () => {
-    const searchText = document.getElementById('search-field').value;
-
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  searchField.value = '';
+  if (searchText == '') {
+    const searchResult = document.getElementById('fish-info');
+    searchResult.innerHTML = `<p class="text-danger mx-auto">Please write something to display</p>`;
+  } else {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
 
     fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.meals))
+      .then(res => res.json())
+      .then(data => displaySearchResult(data.meals))
+  }
+
 }
+
+
+
+// showing search result
 const displaySearchResult = meals => {
-    const searchResult = document.getElementById('fish-info');
-    searchResult.textContent = '';
-    meals.forEach(meal => {
+  const searchResult = document.getElementById('fish-info');
+  searchResult.textContent = '';
+  meals.forEach(meal => {
 
-        const div = document.createElement('div');
-        div.classList.add('col')
-        div.innerHTML = `
-        <div class="card">
-        <img width="200px" src="${meal.strMealThumb}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${meal.strMeal}</h5>
-          <p class="card-text">${meal.strInstructions.slice(0, 200)}.</p>
-        </div>
-      </div>
-        `;
-        searchResult.appendChild(div);
+    const div = document.createElement('div');
+    div.classList.add('col')
+    div.innerHTML = `
+            <div class="card">
+            <img width="200px" src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">${meal.strInstructions.slice(0, 200)}.</p>
+            </div>
+          </div>
+            `;
+    searchResult.appendChild(div);
 
-    })
+  })
 }
 
